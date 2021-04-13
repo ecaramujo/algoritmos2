7c11598eb995755c73d41408b1850e9271d05658
ecaramujo <80298792+ecaramujo@users.noreply.github.com>
2021-04-12 21:37:33 -0300

    Update main.py

--- Exercicio 12-04 Pessoa - EUGENIO ARAUJO/main.py
+++ Exercicio 12-04 Pessoa - EUGENIO ARAUJO/main.py
@@ -6,7 +6,7 @@ from Juridica import Juridica
 print("---Somente Pessoa---")
 jonas = Pessoa(111, "Jonas", "Ipanema", "51-9999")
 jonas.imprimeNome()
-jonas.imprimeTelefone()
+
 
 print("---Somente Pessoa Fisica---")
 jonasfisico = Fisica(222, "JonasFisico", "Cavalhada", "52-9999", "023.849", "25", 62.5, 1.66)
